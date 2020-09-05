39f49bce9722581a6b80b764569f84039e7e1893
Krzysztof Druciarek <kkd26@cam.ac.uk>
2020-09-05 20:25:26 +0200

    Updates

---
+++ precache-manifest.9829c2d7d44c5f47500dd6dda0e1226b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52b1cec7f3ef8958f01b46d5901f162",
+    "url": "/16puzzle/index.html"
+  },
+  {
+    "revision": "aedb9206075f05c6ec90",
+    "url": "/16puzzle/static/css/main.f2d08081.chunk.css"
+  },
+  {
+    "revision": "47c94f4793a280b6fb9b",
+    "url": "/16puzzle/static/js/2.41d9f125.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/16puzzle/static/js/2.41d9f125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedb9206075f05c6ec90",
+    "url": "/16puzzle/static/js/main.3c95c045.chunk.js"
+  },
+  {
+    "revision": "35ea315e5bf33254c2a7",
+    "url": "/16puzzle/static/js/runtime-main.15143fa0.js"
+  }
+]);
