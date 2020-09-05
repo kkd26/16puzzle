17dda80eae39c8b25473ba87ef6377c2a73ecf68
Krzysztof Druciarek <kkd26@cam.ac.uk>
2020-09-05 14:26:44 +0200

    Rename the relative path to file

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/16Puzzle/docs/precache-manifest.ce0f83a403a5eac2dbee1429a445fd57.js"
+  "/16Puzzle/precache-manifest.3c6d560b130012ca289c281c9b843768.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/16Puzzle/docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/16Puzzle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
