656a7553220dc166a3675fde6be7bd25ff888915
Krzysztof Druciarek <kkd26@cam.ac.uk>
2020-09-05 14:20:10 +0200

    Rename build to docs

---
+++ docs/precache-manifest.ce0f83a403a5eac2dbee1429a445fd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d13c2b39942fe1354e070307e280d7",
+    "url": "/16Puzzle/docs/index.html"
+  },
+  {
+    "revision": "8a240388bf3d225fb988",
+    "url": "/16Puzzle/docs/static/css/main.6591d3e7.chunk.css"
+  },
+  {
+    "revision": "d69d98521741ce9832a2",
+    "url": "/16Puzzle/docs/static/js/2.e2005790.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/16Puzzle/docs/static/js/2.e2005790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a240388bf3d225fb988",
+    "url": "/16Puzzle/docs/static/js/main.5792cf2d.chunk.js"
+  },
+  {
+    "revision": "54255c035684eb316406",
+    "url": "/16Puzzle/docs/static/js/runtime-main.97382a18.js"
+  }
+]);
